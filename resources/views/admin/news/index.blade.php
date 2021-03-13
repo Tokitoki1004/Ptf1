0405c5d03042b7055809e26724ab20290cd81738
Tokitoki1004 <daiki.0831.yama.pan@gmail.com>
2021-03-13 20:41:16 +0900

    add_news_edit

--- resources/views/admin/news/index.blade.php
+++ resources/views/admin/news/index.blade.php
@@ -7,17 +7,17 @@
             <h2>ニュース一覧</h2>
         </div>
         <div class="row">
-            <div class="col-md-4">
+            <div class="col-sm-2 col-md-4">
                 <a href="{{ action('Admin\NewsController@add') }}" role="button" class="btn btn-primary">新規作成</a>
             </div>
-            <div class="col-md-8">
+            <div class="col-sm-10 col-md-8">
                 <form action="{{ action('Admin\NewsController@index') }}" method="get">
                     <div class="form-group row">
-                        <label class="col-md-2">タイトル</label>
-                        <div class="col-md-8">
+                        <label class="col-sm-2 col-md-2">タイトル</label>
+                        <div class="col-sm-6 col-md-8">
                             <input type="text" class="form-control" name="cond_title" value="{{ $cond_title }}">
                         </div>
-                        <div class="col-md-2">
+                        <div class="col-sm-2 col-md-2">
                             {{ csrf_field() }}
                             <input type="submit" class="btn btn-primary" value="検索">
                         </div>
